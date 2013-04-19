3f423ac63de068d62586dd76c0e7c2529d3ce3e5
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-04-19 15:20:53 +0200

    Add an helper for inheritance (taken from backbone.js).

--- util.js
+++ util.js
@@ -4,11 +4,11 @@ if (typeof Substance.util === 'undefined') Substance.util = {};
 (function(util){
 
 var env = (typeof exports === 'undefined') ? 'composer' : 'hub';
-var fs = (env == 'hub') ? require('fs') : fs;
 
-/* TODO: underscore can not be found here?
-var _ = (env == 'hub') ? require('underscore') : _;
-*/
+if (typeof exports !== 'undefined') {
+  var fs = require('fs');
+  var _ = (env == 'hub') ? require('underscore') : _;
+}
 
 // Async Control Flow for the Substance
 // --------
@@ -177,6 +177,51 @@ util.propagate = function(data, cb) {
     cb(null, data);
   }
 }
+
+
+
+// shamelessly stolen from backbone.js:
+// Helper function to correctly set up the prototype chain, for subclasses.
+// Similar to `goog.inherits`, but uses a hash of prototype properties and
+// class properties to be extended.
+var ctor = function(){};
+util.inherits = function(parent, protoProps, staticProps) {
+  var child;
+
+  // The constructor function for the new subclass is either defined by you
+  // (the "constructor" property in your `extend` definition), or defaulted
+  // by us to simply call the parent's constructor.
+  if (protoProps && protoProps.hasOwnProperty('constructor')) {
+    child = protoProps.constructor;
+  } else {
+    child = function(){ parent.apply(this, arguments); };
+  }
+
+  // Inherit class (static) properties from parent.
+  _.extend(child, parent);
+
+  // Set the prototype chain to inherit from `parent`, without calling
+  // `parent`'s constructor function.
+  ctor.prototype = parent.prototype;
+  child.prototype = new ctor();
+
+  // Add prototype properties (instance properties) to the subclass,
+  // if supplied.
+  if (protoProps) _.extend(child.prototype, protoProps);
+
+  // Add static properties to the constructor function, if supplied.
+  if (staticProps) _.extend(child, staticProps);
+
+  // Correctly set child's `prototype.constructor`.
+  child.prototype.constructor = child;
+
+  // Set a convenience property in case the parent's prototype is needed later.
+  child.__super__ = parent.prototype;
+
+  return child;
+};
+
+
 
 // Util to read seed data from file system
 // ----------
