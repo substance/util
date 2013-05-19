c116d767114e215342e27ca7f2cd50cec5362835
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-05-19 22:25:29 +0200

    Factored seed stuff from utils.

---
+++ seeds.js
@@ -0,0 +1,159 @@
+(function() {
+
+var root = this;
+
+if (typeof exports !== 'undefined') {
+  var _    = require('underscore');
+  var util   = require('./util');
+} else {
+  var _ = root._;
+  var util = root.Substance.util;
+}
+
+var seeds = {};
+
+var SEEDS_DIR = "./tests/seeds";
+
+seeds.prepareSeedSpec = function(seedSpec, cb) {
+    var _seedSpec = {}
+
+    _seedSpec.localFiles = _.isArray(seedSpec.local) ? seedSpec.local : ((seedSpec.local) ? [seedSpec.local] : []);
+    _seedSpec.remoteFiles = _.isArray(seedSpec.remote) ? seedSpec.remote : ((seedSpec.remote) ? [seedSpec.remote] : []);
+    _seedSpec.requires = _.isArray(seedSpec.requires) ? seedSpec.requires : ((seedSpec.requires) ? [seedSpec.requires] : []);
+    _seedSpec.hubFile = seedSpec.hub;
+    _seedSpec.hub = {};
+    _seedSpec.local = {};
+    _seedSpec.remote = {};
+    _seedSpec.dir = seedSpec.dir;
+
+    cb(null, _seedSpec);
+}
+
+seeds.loadSeedSpec = function (seedName, cb) {
+
+  //console.log("Loading spec...", seedName, data);
+  var location = [SEEDS_DIR, seedName, 'seed.json'].join('/');
+  util.getJSON(location, function(err, seedSpec) {
+    if (err) return cb(err);
+    // storing the file info into the seed spec
+    seedSpec.dir = [SEEDS_DIR, seedName].join('/');;
+    seeds.prepareSeedSpec(seedSpec, cb);
+  });
+}
+
+seeds.loadStoreSeed = function(seedFile, dir, cb) {
+  if (arguments.length == 2) {
+    cb = dir;
+    dir = undefined;
+  }
+
+  var seedsDir = dir || SEEDS_DIR;
+  var location = [seedsDir, seedFile].join('/');
+
+  util.getJSON(location, cb);
+}
+
+seeds.loadSeed = function(seedSpec, cb) {
+
+  var seedsDir = seedSpec.dir || SEEDS_DIR;
+
+  var seed = {
+    local: {},
+    remote: {},
+    hub: {}
+  };
+
+  // Seed specs can contain references to other seed specs in the `requires` field.
+  // This function loads these seeds recursively
+  var loadRequiredSeeds = util.async.iterator({
+    selector: function() { return seedSpec.requires; },
+    iterator: function(seedName, idx, seedSpec, cb) {
+      if (!seedName) return cb(null, seedSpec);
+
+      var spec;
+      var otherSeed;
+
+      function loadSeedSpec(cb) {
+        console.log("Loading referenced seed", seedName);
+        seeds.loadSeedSpec(seedName, function(err, data) {
+          spec = data;
+          cb(err);
+        });
+      }
+
+      function load(cb) {
+        seeds.loadSeed(spec, function(err, data) {
+          console.log("... seed data:", data);
+          otherSeed = data;
+          cb(err);
+        });
+      }
+
+      function store(cb) {
+        _.extend(seed.hub, otherSeed.hub);
+        _.extend(seed.local, otherSeed.local);
+        _.extend(seed.remote, otherSeed.remote);
+        cb(null);
+      }
+
+      util.async.sequential([loadSeedSpec, load, store], cb);
+    }
+  });
+
+  function loadHubSeed(cb) {
+    if (!seedSpec.hubFile) return cb(null);
+
+    console.log("Loading hub seed", seedSpec.hubFile);
+    seeds.loadStoreSeed(seedSpec.hubFile, seedsDir, function(err, hubSeed) {
+      if (err) return cb(err);
+      _.extend(seed.hub, hubSeed);
+      cb(null);
+    });
+  }
+
+  var loadLocalStoreSeeds = util.async.iterator({
+    selector: function() { return seedSpec.localFiles; },
+    iterator: function(resourceName, cb) {
+      if (!resourceName) return cb(null);
+
+      console.log("Loading local seed", resourceName);
+      seeds.loadStoreSeed(resourceName, seedsDir, function(err, storeSeed) {
+        if (err) return cb(err);
+        _.extend(seed.local, storeSeed);
+        cb(null);
+      });
+    }
+  });
+
+  var loadRemoteStoreSeeds = util.async.iterator({
+    // before: function(seed) { console.log("Loading remote store seeds for", seedName); },
+    selector: function() { return seedSpec.remoteFiles; },
+    iterator: function(resourceName, cb) {
+      if (!resourceName) return cb(null);
+
+      console.log("Loading remote seed", resourceName);
+      seeds.loadStoreSeed(resourceName, seedsDir, function(err, storeSeed) {
+        if (err) return cb(err);
+        _.extend(seed.remote, storeSeed);
+        cb(null);
+      });
+    }
+  });
+
+  util.async.sequential([loadRequiredSeeds, loadHubSeed,
+    loadLocalStoreSeeds, loadRemoteStoreSeeds], function(err) {
+      if (err) return cb(err);
+      cb(null, seed);
+    });
+};
+
+// Export Module
+// --------
+
+if (typeof exports === 'undefined') {
+  root.Substance.seeds = seeds;
+} else {
+  module.exports = seeds;
+}
+
+}).call(this);
