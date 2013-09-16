ddc7d7d6998a327b5c4c349b6b434c70f0325aff
Michael Aufreiter <michael@substance.io>
2013-09-16 12:30:43 +0200

    No handlebars.

--- src/html.js
+++ src/html.js
@@ -1,25 +1,24 @@
 "use strict";
 
 var html = {};
-var Handlebars = require("handlebars");
 var _ = require("underscore");
 
 html.templates = {};
 
-html.compileTemplate = function(tplName) {
-  var rawTemplate = $('script[name='+tplName+']').html();
-  html.templates[tplName] = Handlebars.compile(rawTemplate);
-};
+// html.compileTemplate = function(tplName) {
+//   var rawTemplate = $('script[name='+tplName+']').html();
+//   html.templates[tplName] = Handlebars.compile(rawTemplate);
+// };
 
 html.renderTemplate = function(tplName, data) {
   return html.templates[tplName](data);
 };
 
-Handlebars.registerHelper('ifelse', function(cond, textIf, textElse) {
-  textIf = Handlebars.Utils.escapeExpression(textIf);
-  textElse  = Handlebars.Utils.escapeExpression(textElse);
-  return new Handlebars.SafeString(cond ? textIf : textElse);
-});
+// Handlebars.registerHelper('ifelse', function(cond, textIf, textElse) {
+//   textIf = Handlebars.Utils.escapeExpression(textIf);
+//   textElse  = Handlebars.Utils.escapeExpression(textElse);
+//   return new Handlebars.SafeString(cond ? textIf : textElse);
+// });
 
 if (typeof window !== "undefined") {
   // A fake console to calm down some browsers.
