044ae3ae683425560aedfd7339e2fe487071a873
Oliver Buchtala <oliver.buchtala@googlemail.com>
2013-11-28 21:08:45 +0100

    Extracted Fragmenter from Document.Annotator.
    This class is in general helpful for creating valid hierarchically
    structured data (such as XML) in presence of overlapping
    elements.

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ var util = require("./src/util");
 util.errors = require("./src/errors");
 util.html = require("./src/html");
 util.dom = require("./src/dom");
+util.Fragmenter = require("./src/fragmenter");
 
 module.exports = util;
