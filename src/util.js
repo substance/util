3a1be9d1caf9c0b5c4e7bb82b797590017c8224e
Michael Aufreiter <michael@substance.io>
2014-04-28 19:35:03 +0200

    Video killed the radio star.

--- src/util.js
+++ src/util.js
@@ -543,6 +543,19 @@ util.slug = function(str) {
   return str;
 };
 
+
+util.getReadableFileSizeString = function(fileSizeInBytes) {
+
+    var i = -1;
+    var byteUnits = [' kB', ' MB', ' GB', ' TB', 'PB', 'EB', 'ZB', 'YB'];
+    do {
+        fileSizeInBytes = fileSizeInBytes / 1024;
+        i++;
+    } while (fileSizeInBytes > 1024);
+
+    return Math.max(fileSizeInBytes, 0.1).toFixed(1) + byteUnits[i];
+};
+
 // Export
 // ====
 
