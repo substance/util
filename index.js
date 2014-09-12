0e0bf5f3b299b987e1657e2dd8a702c2b45dad94
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-09-12 23:46:08 +0200

    Get rid of stuff.

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ var util = require("./src/util");
 
 util.async = require("./src/async");
 util.errors = require("./src/errors");
-util.html = require("./src/html");
-util.dom = require("./src/dom");
-util.zip = require("./src/zip");
-util.Fragmenter = require("./src/fragmenter");
 
 module.exports = util;
