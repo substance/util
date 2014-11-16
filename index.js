ba48be41078921e49090d0123b31f8b6e55bd155
Oliver Buchtala <oliver.buchtala@gmail.com>
2014-11-16 20:40:13 +0100

    Integrated substance-regexp.

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ var util = require("./src/util");
 util.errors = require("./src/errors");
 util.html = require("./src/html");
 util.dom = require("./src/dom");
+util.RegExp = require("./src/regexp");
 
 module.exports = util;
