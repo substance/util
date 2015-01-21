6f7c9bcde3d45a51319cf3bfee02a6e2a4cd5f84
Oliver Buchtala <oliver.buchtala@gmail.com>
2015-01-21 03:13:24 +0100

    Added fragmenter for legacy reason.

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ var util = require("./src/util");
 
 util.async = require("./src/async");
 util.errors = require("./src/errors");
+util.Fragmenter = require('./src/fragmenter');
 
 module.exports = util;
